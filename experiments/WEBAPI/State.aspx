9f743b7ff0d134af315435edf3212e81bc83a1b0
abhishek10 <kedari.a@husky.neu.edu>
2015-03-29 16:06:31 -0400

    Added week-9 experiments
    Added week-9 experiments for calling external web APIs

---
+++ experiments/WEBAPI/State.aspx
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Location</title>
+  <script type="text/javascript" src="http://api.eventful.com/js/api"></script>
+  <script type="text/javascript" src="../../javascript/week8.js"></script>
+</head>
+<body>
+<div>
+ <h2>About the Experiment</h2> 
+  <p>Calling external web API to get state of the event</p>
+</div>
+<hr />
+<div>
+     <h2>Demo</h2>
+    Event Name - Mamma Mia <br><br>
+    <input type="button" onclick="get_state()" value="Get State" /> <br>
+</div>
+<hr />
+    <div>
+       <h2>Code Snippet</h2> 
+       <p>
+function get_location() {
+    var oArgs = {
+        app_key: &quot;MkPtkR7vPVRNxjVh&quot;,
+        q: &quot;music&quot;,
+        where: &quot;San Diego&quot;,
+        &quot;date&quot;: &quot;2013061000-2015062000&quot;,
+        page_size: 5,
+        sort_order: &quot;popularity&quot;,
+    };
+    EVDB.API.call(&quot;/events/search&quot;, oArgs, function (oData) {
+    alert(oData.events.event[0].region_name);
+    });
+}
+       </p>
+    </div>
+    <hr />
+
+    <div>
+       <h2>Link to source code</h2> 
+       <a href="../../fileview/Default.aspx?~/experiments/WEBAPI/State.aspx" target="_blank">View Source</a><br>
+       <a href="../../fileview/Default.aspx?~/javascript/week8.js" target="_blank">View java script Source</a>
+    </div>
+    <hr />
+
+    <div>
+       <h2>References</h2> 
+        <p><a href="http://api.eventful.com/" target="_blank">www.evenful.com/</a></p>
+    </div>
+    <hr />
+</body>
+</html>
---
+++ javascript/week8.js
@@ -0,0 +1,70 @@
+﻿
+function get_location() {
+    var oArgs = {
+        app_key: "MkPtkR7vPVRNxjVh",
+        q: "music",
+        where: "San Diego",
+        "date": "2013061000-2015062000",
+        page_size: 5,
+        sort_order: "popularity",
+    };
+    EVDB.API.call("/events/search", oArgs, function (oData) {
+    alert(oData.events.event[0].venue_address + " - " + oData.events.event[0].city_name);
+    });
+}
+
+function get_Time() {
+    var oArgs = {
+        app_key: "MkPtkR7vPVRNxjVh",
+        q: "music",
+        where: "San Diego",
+        "date": "2013061000-2015062000",
+        page_size: 5,
+        sort_order: "popularity",
+    };
+    EVDB.API.call("/events/search", oArgs, function (oData) {
+        alert(oData.events.event[0].start_time);
+    });
+}
+
+function get_venue_location() {
+    var oArgs = {
+        app_key: "MkPtkR7vPVRNxjVh",
+        q: "music",
+        where: "San Diego",
+        "date": "2013061000-2015062000",
+        page_size: 5,
+        sort_order: "popularity",
+    };
+    EVDB.API.call("/events/search", oArgs, function (oData) {
+    alert(oData.events.event[0].venue_name);
+    });
+}
+
+function get_creator() {
+    var oArgs = {
+        app_key: "MkPtkR7vPVRNxjVh",
+        q: "music",
+        where: "San Diego",
+        "date": "2013061000-2015062000",
+        page_size: 5,
+        sort_order: "popularity",
+    };
+    EVDB.API.call("/events/search", oArgs, function (oData) {
+    alert(oData.events.event[0].performers.performer.creator);
+    });
+}
+
+function get_state() {
+    var oArgs = {
+        app_key: "MkPtkR7vPVRNxjVh",
+        q: "music",
+        where: "San Diego",
+        "date": "2013061000-2015062000",
+        page_size: 5,
+        sort_order: "popularity",
+    };
+    EVDB.API.call("/events/search", oArgs, function (oData) {
+        alert(oData.events.event[0].region_name);
+    });
+}
